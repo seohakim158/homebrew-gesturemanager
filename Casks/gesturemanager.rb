f9e540ae689f5f9aec47ec27a671110d2c25b79b
seohakim158 <163643988+seohakim158@users.noreply.github.com>
2026-07-11 23:12:50 +0900

    Fix cask

--- Casks/gesturemanager.rb
+++ Casks/gesturemanager.rb
@@ -1,6 +1,6 @@
 cask "gesturemanager" do                                                                                                                                                                                                                 ─╯
   version "1.0"
-  sha256 "05528nano Casks/gesturemanager.rb0fe8b95e38a0e59b6a6be691c4239a94012e31c673910ac995269396034"
+  sha256 "055280fe8b95e38a0e59b6a6be691c4239a94012e31c673910ac995269396034"
 
   url "https://github.com/seohakim158/GestureManager/releases/download/v1.0/GestureManager.dmg"
 
